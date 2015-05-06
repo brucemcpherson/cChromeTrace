0bc9ba4a16b5d095c82f4b0df7e5e7952c5ee134
Bruce McPherson <bruce@mcpher.com>
2015-05-06 10:54:09 +0100

    updated by GasGit automation

--- scripts/forPublishing.js
+++ scripts/forPublishing.js
@@ -7,7 +7,7 @@ function getLibraryInfo () {
   return { 
     info: {
       name:'cChromeTrace',
-      version:'0.0.1',
+      version:'0.0.2',
       key:'M6SHE8Ysn-5KyFf8NGzQpTSz3TLx7pV4j',
       description:'enable chrome tracing from apps script',
       share:'https://script.google.com/d/18DUgAypAyUNTeHmCVDHtiqrTFc9ovMKFQ70bFOMgJrB-ho49Z0UC-utu/edit?usp=sharing'
